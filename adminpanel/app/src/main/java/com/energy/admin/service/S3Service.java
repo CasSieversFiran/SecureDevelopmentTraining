b92c282ba3cda9d44bade0c6f5af66344d66c06b
Cas Sievers <cas.sievers@firan.nl>
2025-10-02 14:29:00 +0200

    Update S3Service.java

--- adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
+++ adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
@@ -16,8 +16,8 @@ public class S3Service {
     private final S3Client s3Client;
 
     public S3Service() {
-        String aws_access_key_id = "AKIA<placeholder>";
-        String aws_secret_access_key = "<placeholder>";
+        String aws_access_key_id = "AKIAAKIA2T2SJH6MTEAZWM6F";
+        String aws_secret_access_key = "7n/CbgY/92TMIDgrSwSnkk4m9bvukdbEV2fKSTVB";
  
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(aws_access_key_id, aws_secret_access_key);
 
